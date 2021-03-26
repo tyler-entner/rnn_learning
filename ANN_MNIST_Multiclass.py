1e9ab88c6de654c8dc4fbc694612e3118fdbc71a
tyler-entner <tyler.entner@gmail.com>
2021-03-26 10:03:48 -0400

    Added updated multiclass predictor

--- ANN_MNIST_Multiclass.py
+++ ANN_MNIST_Multiclass.py
@@ -6,3 +6,98 @@ Created on Fri Mar 26 10:01:32 2021
 """
 
 
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+#Load in MNIST dataset and preprocess
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train/255.0, x_test/255.0
+
+print("x_train.shape: ", x_train.shape)
+
+#Build feedforward NN
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape = (28, 28)),
+  tf.keras.layers.Dense(128, activation = 'relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10, activation = 'softmax')
+])
+
+#Compile the model
+model.compile(optimizer = 'adam',
+              loss = 'sparse_categorical_crossentropy',
+              metrics = ['accuracy'])
+
+#Train the model
+r = model.fit(x_train, y_train, validation_data = (x_test, y_test), epochs = 10)
+
+#Plot loss to evaluate the model
+plt.plot(r.history["loss"], label = "loss")
+plt.plot(r.history["val_loss"], label = "val_loss")
+plt.legend()
+
+
+#Plot accuracy to evaluate
+plt.plot(r.history["accuracy"], label = "acc")
+plt.plot(r.history["val_accuracy"], label = "val_acc")
+plt.legend()
+
+#Evaluate the model
+print(model.evaluate(x_test, y_test))
+
+#Create and plot confusion matrix
+from sklearn.metrics import confusion_matrix
+import numpy as np
+import itertools
+
+#Create function to plot confusion matrix
+def plot_confusion_matrix(cm, classes,
+                          normalize = False,
+                          title = 'Confusion Matrix',
+                          cmap = plt.cm.Blues):
+  
+  """
+  This function is used to print and plot the 
+  confusion matrix. 
+  """
+
+  if normalize:
+    cm = cm.astype('float')/cm.sum(axis=1)[:, np.newaxis]
+    print("Normalized confusion matrix...")
+  else:
+    print('Confusion matrix, without normalization...')
+  print(cm)
+
+  plt.imshow(cm, interpolation = 'nearest', cmap = cmap)
+  plt.title(title)
+  plt.colorbar()
+  tick_marks = np.arange(len(classes))
+  plt.xticks(tick_marks, classes)
+  plt.yticks(tick_marks, classes)
+
+  fmt = '.2f' if normalize else 'd'
+  thresh = cm.max() / 2.
+  for i,j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    plt.text(j, i, format(cm[i,j], fmt), 
+             horizontalalignment = "center",
+             color = "white" if cm[i,j] > thresh else "black")
+    
+  plt.tight_layout()
+  plt.ylabel('True Label')
+  plt.xlabel('Predicted Label')
+  plt.show()
+
+#Show confusion matix on predicted data
+p_test = model.predict(x_test).argmax(axis=1)
+cm = confusion_matrix(y_test, p_test)
+plot_confusion_matrix(cm, list(range(10)))
+
+
+#Show misclassified examples
+misclassified_idx = np.where(p_test != y_test)[0]
+i = np.random.choice(misclassified_idx)
+plt.imshow(x_test[i], cmap = 'gray')
+plt.title("True label: %s Predicted: %s" % (y_test[i], p_test[i]));
